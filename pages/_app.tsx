321ad53416a2cc68c3feca720acc9695cac08682
fauzanr <fauzanr17@gmail.com>
2023-01-24 17:35:03 +0700

    cleanup, add navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Navbar from "@/components/ui/navbar";
+import { Rubik } from "@next/font/google";
+
+const font = Rubik({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={font.className}>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
