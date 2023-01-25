173fdf1263b36533057a4739b620289c6e37e3a8
fauzanr <fauzanr17@gmail.com>
2023-01-25 17:04:12 +0700

    prerender geist ui global css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { CssBaseline } from "@geist-ui/core";
 
 export default function Document() {
+  const geistUIcss = CssBaseline.flush();
   return (
     <Html lang="en">
-      <Head />
+      <Head>{geistUIcss}</Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
